ad05be1c083a9212041a147cb6c94598251767ad
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-17 22:27:39 +0700

    Add GPX nodes which are too far away from existing OSM nodes to the map

--- scripts/create-osm.py
+++ scripts/create-osm.py
@@ -20,13 +20,33 @@ def create_osm(df, osm_file):
     osm.set("attribution", "http://www.openstreetmap.org/copyright")
     osm.set("license", "http://opendatacommons.org/licenses/odbl/1-0/")
 
+    current_time = datetime.now()
+    timestamp = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    nodes_df = df\
+    .drop_duplicates(subset=['node_id'])\
+    .sort_values(by='node_id')
+
+    for _, row in nodes_df.iterrows():
+        node_id = row["node_id"]
+        
+        is_fake_node = row["fake_node"]
+        if is_fake_node:
+            ET.SubElement(
+                osm,
+                "node",
+                id=str(node_id),
+                lat=str(row["lat"]),
+                lon=str(row["lon"]),
+                visible="true",
+                version="1", 
+                timestamp=timestamp,       
+            )
+
     tags = [
         {"k": "highway", "v": "footway"},
         {"k": "foot", "v": "designated"},
     ]
-
-    current_time = datetime.now()
-    timestamp = current_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
     ways_df = df.groupby('filename')['node_id'].agg(lambda x: list(x.unique())).reset_index()
     
@@ -76,20 +96,21 @@ for csv_file in csv_files:
   filename = get_filename(csv_file)
   
   df = pd.read_csv(csv_file)
+  
   df['filename'] = filename
-  
-  # Remove matched nodes that are too far
-  df = df[df['distance'] <= 10]
-  
   df['nodes'] = df['nodes'].apply(ast.literal_eval)
   
-  # Split nodes into separate rows
   df = df.explode('nodes').reset_index(drop=True)
   df = df.rename(columns={'nodes': 'node_id'})
   
-  node_dfs.append(df[['node_id', 'filename']])
+  node_dfs.append(df)
 
 all_node_dfs = pd.concat(node_dfs).reset_index(drop=True)
+
+# Create new nodes for which no nearby OSM node exists
+mask = all_node_dfs['distance'] > 4
+all_node_dfs.loc[mask, 'node_id'] = range(1, mask.sum() + 1)
+all_node_dfs['fake_node'] = mask
 
 create_osm(
   all_node_dfs,
