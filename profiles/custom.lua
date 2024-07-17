0c846c9c919836fcbc625b7d49e605049e798a79
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-07-17 14:53:46 +0700

    Custom route is now different from the shortest one

--- profiles/custom.lua
+++ profiles/custom.lua
@@ -8,7 +8,7 @@ Handlers = require("lib/way_handlers")
 find_access_tag = require("lib/access").find_access_tag
 
 function setup()
-  custom_route_bonus                = 10  -- Bonus factor for GPX route paths
+  custom_route_bonus                = 100  -- Bonus factor for GPX route paths
 
   walking_speed                     = 5
 
